0c8b234df2ab248e84ca5592df60025b55b81fda
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-08-06 20:01:24 +0100

    refactored the whole saved book

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 import {
   Jumbotron,
   Container,
@@ -7,80 +9,41 @@ import {
   Button,
 } from "react-bootstrap";
 
-import Auth from "../utils/auth";
-import { useQuery } from "@apollo/client";
-import { removeBookId } from "../utils/localStorage";
 import { ME } from "../queries";
 import { REMOVE_BOOK } from "../mutation";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
-
+  // use query hook for the me query and get the data, error and loading state from graphQL
   const { data, error, loading } = useQuery(ME);
 
-  const [REMOVE_BOOK] = useMutation(REMOVE_BOOK);
-  // use this to determine if `useEffect()` hook needs to run again
-  // const userDataLength = Object.keys(userData).length;
+  // use mutation hook for the removeBook mutation and pass functions to handle success and error
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error("something went wrong!");
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
-
-  const userData = data.me;
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
     try {
       await removeBook({
         variables: {
           removeBookBookId: bookId,
         },
       });
-      // const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
+  // if state is loading
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
+
+  // if state is error
+  if (error) {
+    return <h2>ERROR...</h2>;
+  }
+
+  // get the data from the graphql request
+  const userData = data.me;
 
   return (
     <>
